591cea8b14d94320f1937de7b55229dcd8d2a907
root <mb645@njit.edu>
2019-12-02 18:31:29 -0500

    unzip

--- installer.sh
+++ installer.sh
@@ -1,8 +1,26 @@
 #!/bin/bash
 filename=$1
-location=$2
-tar xvzf $filename -C $location
 
-#First parameter is the tar file name and location
-#Second parameter is the location to extract it to
-#For example: ./installer.sh /home/mb/Desktop/backendSample.tar.gz /home/mb/git/installertest
+unzip -o ./bin/bundle.zip sim-meta.json -d ./bin/
+
+for ((i=0;i<$(jq '. | length' ./bin/sim-meta.json);i++))
+        do
+                var1=$(jq -j .[$i]'["folderName"]' ./bin/sim-meta.json)
+		echo $var1
+                var2=$(jq -j .[$i]'["unpackTo"]' ./bin/sim-meta.json)
+		echo $var2
+
+		#if [ $var2 = "/Desktop/" ]
+	       	#then
+		#	unzip -o $filename $var1/* -d ~/Desktop/
+		#else
+               	#	unzip -o $filename $var1/* -d $var2
+		#fi
+
+		unzip -o $filename $var1/* -d $var2
+        done
+
+#To run use the following command:
+#./installer.sh ./bin/bundle.zip
+#
+#If extracting to Desktop, make sure to either include full path in json file such as "/home/mb/Desktop/" or use "/Desktop/" and unquote the if loop I wrote, then remove the second unzip command. For some reason "~/Desktop/" or just "Desktop" by itself will not work when using a json file variable. 
