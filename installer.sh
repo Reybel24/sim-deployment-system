5430647056015f8aa6026a02766721f1d24d9447
Marcin <mb645@njit.edu>
2019-12-18 15:22:41 -0500

    nagios nrpe

--- installer.sh
+++ installer.sh
@@ -6,19 +6,24 @@ unzip -o ./bin/bundle.zip sim-meta.json -d ./bin/
 for ((i=0;i<$(jq '. | length' ./bin/sim-meta.json);i++))
         do
                 var1=$(jq -j .[$i]'["folderName"]' ./bin/sim-meta.json)
-		echo $var1
+                echo $var1
                 var2=$(jq -j .[$i]'["unpackTo"]' ./bin/sim-meta.json)
-		echo $var2
+                echo $var2
+                #var3=~/Desktop/
+                #echo $var2$var3
 
-		#if [ $var2 = "/Desktop/" ]
-	       	#then
-		#	unzip -o $filename $var1/* -d ~/Desktop/
-		#else
-               	#	unzip -o $filename $var1/* -d $var2
-		#fi
 
-		unzip -o $filename $var1/* -d $var2
+                #if [ $var2 = "/Desktop/" ]
+                #then
+                #       unzip -o $filename $var1/* -d ~/Desktop/
+                #else
+                #       unzip -o $filename $var1/* -d $var2
+                #fi
+
+                unzip -o $filename $var1/* -d /home/$USER$var2
+
         done
+
 
 #To run use the following command:
 #./installer.sh ./bin/bundle.zip
